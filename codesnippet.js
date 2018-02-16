5ce158198bd97f3ffaf6293e74409190a95098b3
Mr-Ingram <35975325+Mr-Ingram@users.noreply.github.com>
2018-02-16 14:33:25 -0600

    Create codesnippet.js

---
+++ codesnippet.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, name: "Francesco Raoux", city: 'Gulfport' },
+  { id: 2, name: "Tasha Bonanno" },
+  { id: 3, name: "Merle Braunstein" },
+  { id: 4, name: "Aleda Bouzan" },
+  { id: 5, name: "Issiah Elnaugh" }
+];
