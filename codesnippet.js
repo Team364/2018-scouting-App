3e73cff28d6ef8f3f96604457a8d68f588e6a124
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2019-06-25 03:15:53 -0500

    clears some clutter

--- codesnippet.js
+++
@@ -1,7 +0,0 @@
-const data = [
-  { id: 1, name: "Francesco Raoux", city: 'Gulfport' },
-  { id: 2, name: "Tasha Bonanno" },
-  { id: 3, name: "Merle Braunstein" },
-  { id: 4, name: "Aleda Bouzan" },
-  { id: 5, name: "Issiah Elnaugh" }
-];
